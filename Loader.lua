46ef3200348e5f45e2988e33418782d2a0b0d8e6
laderite <78981916+laderite@users.noreply.github.com>
2024-12-12 20:25:37 -0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local scripts = {
+	[6490954291] = "https://api.luarmor.net/files/v3/loaders/9968755f5ae02a057d9208671aa3576b.lua", --// GHOUL://RE
+	[4127666953] = "https://api.luarmor.net/files/v3/loaders/e70c05e220610956e958ce7907e774df.lua", --// DEMON HUNTER
+	[2132098792] = "https://api.luarmor.net/files/v3/loaders/44ce714e2ab46ba6769e5852c08209d8.lua", --// HOLLOWED
+}
+
+local GameId = game.GameId
+
+if scripts[GameId] then
+	local script = game:HttpGet(scripts[GameId])
+	if script then
+		loadstring(script)()
+	end
+end
